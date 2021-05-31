1b2c01da2a5778980179d33e52cfd9eff4eb2512
Ellite <ellite@ellite.dev>
2021-05-31 14:26:17 +0200

    added google auth in a horrible way :)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button'
+import firebase from "firebase";
+import "firebase/auth"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+            <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>"app.js"</code> and save to refresh (and cum).
@@ -18,9 +20,54 @@ function App() {
         >
         <Button variant="outline-primary">Learn React</Button>{' '}
         </a>
+
+        <section id="whenSignedOut">
+          <Button id="signInBtn" variant="outline-primary">Sign In with Google</Button>
+        </section>
+
+        <section id="whenSignedIn" hidden="true">
+          <div id="userDetails"></div>
+          <Button id="signOutBtn" variant="outline-danger">Sign Out</Button>
+        </section>
+
       </header>
     </div>
   );
 }
 
-export default App;
+ let isFirebaseAppDefined = false;
+    setInterval(() => {
+      if (!isFirebaseAppDefined) {
+        if (firebase.app()) {
+            const auth = firebase.auth();
+            const whenSignedIn = document.getElementById('whenSignedIn');
+            const whenSignedOut = document.getElementById('whenSignedOut');
+
+            const signInBtn = document.getElementById('signInBtn');
+            const signOutBtn = document.getElementById('signOutBtn');
+
+            const userDetails = document.getElementById('userDetails');
+
+            const provider = new firebase.auth.GoogleAuthProvider();
+
+            signInBtn.onclick = () => auth.signInWithPopup(provider);
+            signOutBtn.onclick = () => auth.signOut();
+
+            auth.onAuthStateChanged(user => {
+              if (user) {
+              // signed in
+                whenSignedIn.hidden = false;
+                whenSignedOut.hidden = true;
+                userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+              } else {
+                    whenSignedIn.hidden = true;
+                whenSignedOut.hidden = false;
+                userDetails.innerHTML = '';
+            }
+            });
+          isFirebaseAppDefined = true;
+        }
+      }
+    }, 100);
+
+console.log(firebase)
