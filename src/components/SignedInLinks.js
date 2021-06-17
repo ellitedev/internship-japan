ad586e9e45e07f27191e3a574ea7fdb7ef22c6ff
Ellite <ellite@ellite.dev>
2021-06-17 14:02:23 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/ellitedev/internship-japan into main""
    This reverts commit 221fc330a53c2c0ad455a3d7c0b4ef03ff600a63.

--- src/components/SignedInLinks.js
+++ src/components/SignedInLinks.js
@@ -1,30 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Button } from "react-bootstrap";
 import { connect } from "react-redux";
 import { signOut } from "../store/actions/authActions";
 import { LinkContainer } from "react-router-bootstrap";
-import config from "../config/fbConfig";
 
 // check out the video i used for this https://youtu.be/j9IT0E835B8
 
 const SignedInLinks = (props) => {
-  const [currentUser, setCurrentUser] = useState();
+  const { auth, profile } = props;
 
-  useEffect(() => {
-    config.auth().onAuthStateChanged((user) => {
-      setCurrentUser(user);
-    });
-  }, []);
+  function Fname() {
+  return(<div>{profile.firstName}</div>)
+  }
+  function Dname() {
+    return(<div>{auth.displayName}</div>)
+  }
+
+  const userName = profile.firstName ? <div><Fname/></div> : <div><Dname/></div>;
 
   return (
     <div>
-      {currentUser && (
         <LinkContainer to="/details">
           <Button variant="outline-info" className="mr-2">
-            {currentUser.displayName}
+            <div>{userName}</div>
           </Button>
         </LinkContainer>
-      )}
       <Button onClick={props.signOut} variant="outline-danger" className="mr-2">
         Log Out
       </Button>
@@ -32,10 +32,18 @@ const SignedInLinks = (props) => {
   );
 };
 
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
+  }
+}
+
 const mapDispatchToProps = (dispatch) => {
   return {
     signOut: () => dispatch(signOut()),
   };
 };
 
-export default connect(null, mapDispatchToProps)(SignedInLinks);
+export default connect(mapStateToProps, mapDispatchToProps)(SignedInLinks);
